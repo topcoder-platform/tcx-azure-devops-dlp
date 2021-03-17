e2bff1db1b352e97b430b63fbaea5be59f2cd51a
Dhruvit Raithatha <dhruvit.raithatha@gmail.com>
2021-03-17 10:52:40 +0530

    For #125: AWS handshake

--- DLPTrigger/handlers/options.ts
+++ DLPTrigger/handlers/options.ts
@@ -1,14 +1,13 @@
 import { Context, HttpRequest } from '@azure/functions'
 import { createHash } from 'crypto'
-import Boom from '@hapi/boom'
 
-export default async function handleHandshakeRequest (
+export default async function handleOptionsRequest (
   _context: Context,
   req: HttpRequest
 ) {
   const handshakeRequestData = req.headers['x-handshake-request-data']
   if (!handshakeRequestData) {
-    throw Boom.badRequest('Missing handshake data header.')
+    return {}
   }
   const digest = createHash('sha256').update(handshakeRequestData).digest('hex')
   const responseHeaders = { 'x-handshake-response-data': digest }
