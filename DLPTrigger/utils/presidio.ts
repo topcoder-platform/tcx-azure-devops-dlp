2d7965fdb885437363a3bf6eddd3bf8506bfdb6c
Dhruvit Raithatha <dhruvit.raithatha@gmail.com>
2021-06-17 06:02:21 +0530

    For #130: Self-signed cert as part of deployment

--- DLPTrigger/utils/presidio.ts
+++ DLPTrigger/utils/presidio.ts
@@ -1,4 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import https from 'https'
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false })
 
 export interface PresidioItem {
   field: {
@@ -11,20 +14,42 @@ export interface PresidioItem {
     length: number
   }
 }
+export type PresidioRes = PresidioItem[]
 
-export type PresidioRes = PresidioItem[] | null
+interface PresidioItemV2 {
+  analysis_explanation: null | string
+  end: number
+  entity_type: string
+  score: number
+  start: number
+}
 
-export async function identifyPII (dataString: string) {
+export type PresidioResV2 = PresidioItemV2[]
+
+export async function identifyPII (dataString: string): Promise<PresidioRes> {
   const requestConfig: AxiosRequestConfig = {
     method: 'POST',
     url: process.env.PRESIDIO_ENDPOINT,
     data: {
       text: dataString,
-      analyzeTemplate: {
-        allFields: true
-      }
-    }
+      language: 'en'
+    },
+    headers: {
+      Host: 'tcx-presidio.svc'
+    },
+    httpsAgent
   }
-  const res = await axios(requestConfig)
-  return res.data as PresidioRes
+  const res = await axios.request<PresidioResV2>(requestConfig)
+  if (res?.data instanceof Array) {
+    return res.data.map(item => ({
+      field: { name: item.entity_type },
+      score: item.score,
+      location: {
+        start: item.start,
+        end: item.end,
+        length: item.end - item.start
+      }
+    }))
+  }
+  return res.data
 }
